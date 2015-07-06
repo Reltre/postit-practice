93ca41753f0ed2d239dea11a72575a4c01b5b188
Reltre <tglippert@gmail.com>
2015-07-06 15:59:50 -0700

    Added time zone functionality for post and user.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
-  def fix_url(url) 
-    url =~ /^https?:\/\//  ? url : "http://#{url}" 
+
+
+  def fix_url(url)
+    url =~ /^https?:\/\//  ? url : "http://#{url}"
   end
 
   def user_friendly_date(datetime)
-    difference = Time.zone.now - datetime
-    distance_of_time_in_words(difference)
+    if logged_in? && !current_user.time_zone.blank?
+      Time.use_zone current_user.time_zone
+    end
+    distance_of_time_in_words_to_now(datetime)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628045854) do
+ActiveRecord::Schema.define(version: 20150630183921) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150628045854) do
     t.string   "password_digest"
     t.string   "slug"
     t.string   "role"
+    t.string   "time_zone"
   end
 
   create_table "votes", force: :cascade do |t|
